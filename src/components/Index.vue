3b0a831580d23579b70a8e2107ab278c32c433fa
Roy <a365039311@gmail.com>
2017-02-13 16:46:38 +0800

    first commit

---
+++ src/components/Index.vue
@@ -0,0 +1,24 @@
+
+<template>
+<div class="index">
+  <hello></hello>
+  <about></about>
+</div>
+</template>
+
+<script>
+import about from 'components/About'
+import hello from 'components/Hello'
+export default {
+  name: 'index',
+  components: {
+    about,
+    hello
+  },
+  data () {
+    return {
+      msg: '欢迎来到千人行工作室'
+    }
+  }
+}
+</script>
